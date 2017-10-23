be0827dbdf38e5b0d16ce966c37b6f375737f35b
Beat Küng <beat-kueng@gmx.net>
2017-10-23 21:34:31 +0200

    uavcan{esc,node}_main: remove unneeded include board_serial.h

--- src/modules/uavcannode/uavcannode_main.cpp
+++ src/modules/uavcannode/uavcannode_main.cpp
@@ -48,7 +48,6 @@
 #include <systemlib/systemlib.h>
 #include <systemlib/param/param.h>
 #include <systemlib/mixer/mixer.h>
-#include <systemlib/board_serial.h>
 #include <version/version.h>
 __BEGIN_DECLS
 #include <nuttx/board.h>
