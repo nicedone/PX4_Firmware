61cbc7b3752e9df58b0065ccc67b62712da767cf
Andreas Antener <antener_a@gmx.ch>
2017-11-26 15:24:00 +0100

    Log rate controller integrators (FW + MC)

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -598,6 +598,7 @@ void Logger::add_default_topics()
 	add_topic("offboard_mission");
 	add_topic("optical_flow", 50);
 	add_topic("position_setpoint_triplet", 200);
+	add_topic("rate_ctrl_status", 200);
 	add_topic("sensor_combined", 100);
 	add_topic("sensor_preflight", 200);
 	add_topic("system_power", 500);
